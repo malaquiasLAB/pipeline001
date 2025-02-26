df48ec6b054a37fd449dc741c1b581775639e44d
Alexandre Marques Menezes Malaquias <a.menezes.malaquias@avanade.com>
2025-02-26 11:00:29 -0300

    Trigando projetos para aguardar aprovação

--- teste.sh
+++ teste.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-deploys=$(cat deploy.json)
+curl -s -L \
+                -H "Accept: application/vnd.github+json" \
+                -H "Authorization: token $PAT" \
+                -H "X-GitHub-Api-Version: 2022-11-28" \
+                https://api.github.com/repos/malaquiasLAB/repo1/actions/runs?branch=main | \
+                jq '.workflow_runs| map(select(.status == "waiting" and .head_branch == "main" and .path == ".github/workflows/workflow.yml" ))' 
 
-echo ::set-output name=DEPLOYS::$deploys
 
-echo $DEPLOYS
 
-echo
-
-echo $deploys
+#curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $PAT }}" https://api.github.com/repos/malaquiasLAB/repo1/actions/runs | jq -r '.workflow_runs[] | select(.name == "workflow.yml") | select(.status == "waiting") | .id' 
